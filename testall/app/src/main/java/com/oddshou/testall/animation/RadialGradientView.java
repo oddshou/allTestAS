f3b061fb8caa56528197e97a2bdc3a565d5593d9
oddshou <oddshou@sina.com>
2017-06-16 18:47:47 +0800

    完成按钮点击波浪效果

--- testall/app/src/main/java/com/oddshou/testall/animation/RadialGradientView.java
+++ testall/app/src/main/java/com/oddshou/testall/animation/RadialGradientView.java
@@ -1,6 +1,6 @@
 package com.oddshou.testall.animation;
 
-import android.animation.ArgbEvaluator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
@@ -14,8 +14,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.oddshou.testall.Logger;
+import com.oddshou.testall.R;
 
 /**
  * Created by win7 on 2017/6/15.
@@ -25,81 +30,110 @@ public class RadialGradientView extends Activity {
 
 
     private static final String TAG = "RadialGradientView";
+    private TestView mTestview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new TestView(this));
+        mTestview = new TestView(this);
+        setContentView(R.layout.activity_container);
+        LinearLayout container = (LinearLayout) findViewById(R.id.container);
+        container.addView(mTestview, 200, 100);
+
+
+        mTestview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RadialGradientView.this, "你点我了", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public class TestView extends View{
+    public class TestView extends View implements ValueAnimator.AnimatorUpdateListener{
         Paint mGradientPaint;
         private static final int START_COLOR = 0X00FFFFFF;
         private static final int END_COLOR = 0xFF58FAAC;
-        float mTouchx, mTouchy;
+        float mTouchx;
+        float mTouchy;
+        float mRadius;
+
+        public int getBalpha() {
+            return balpha;
+        }
+
+        public void setBalpha(int balpha) {
+            this.balpha = balpha;
+        }
+
+        int balpha = 255;
+
+        public float getMRadius() {
+            return mRadius;
+        }
+
+        public void setMRadius(float mRadius) {
+            this.mRadius = mRadius;
+        }
+
+
+        AnimatorSet animationSet;
 
         public TestView(Context context) {
             super(context);
             mGradientPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-//            mGradientPaint.setShader()
-            setBackgroundColor(Color.GRAY);
+            setBackgroundColor(Color.GREEN);
         }
 
         @Override
         public boolean onTouchEvent(MotionEvent event) {
-//            if (event.getAction() != MotionEvent.ACTION_DOWN) {
-////                mTouchx = -1;
-//                return super.onTouchEvent(event);
-//            }
+            if (event.getAction() != MotionEvent.ACTION_DOWN) {
+                return super.onTouchEvent(event);
+            }
 
             mTouchx = event.getX();
             mTouchy = event.getY();
             RadialGradient radialGradient = new RadialGradient(mTouchx, mTouchy, 150,
-                    Color.RED, Color.GREEN, Shader.TileMode.CLAMP);
-            mGradientPaint.setShader(radialGradient);
-            postInvalidate();
+                    0x00dddddd, 0x00FFFFFF, Shader.TileMode.CLAMP);
+//            mGradientPaint.setShader(radialGradient); //shader暂不使用
+            mGradientPaint.setColor(Color.GRAY);
             Logger.i(TAG, "onTouchEvent: " + mTouchx + " mTouchy " + mTouchy, "oddshou");
-
-
+            startAnimation();
             return super.onTouchEvent(event);
         }
+
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            if (mTouchx > 0) {
-                canvas.drawCircle(mTouchx, mTouchy, 50, mGradientPaint);
+            mGradientPaint.setAlpha(balpha);
+            canvas.drawCircle(mTouchx, mTouchy, mRadius, mGradientPaint);
+        }
+
+        private void createAnimation() {
+            if (animationSet == null) {
+                animationSet = new AnimatorSet();
+                ValueAnimator animation1 = ObjectAnimator.ofFloat(this, "mRadius", 0, 200);
+                animation1.setDuration(300);
+                animation1.setInterpolator(new LinearInterpolator());
+                animation1.addUpdateListener(this);
+
+                ValueAnimator animator2 = ObjectAnimator.ofInt(this, "balpha", 255, 0);
+                animator2.setDuration(300);
+                animator2.setInterpolator(new DecelerateInterpolator());
+                animationSet.play(animation1).with(animator2);
+
+
             }
         }
-    }
 
-
-    public class MyAnimationView extends View {
-
-        private static final int RED = 0xffFF8080;
-        private static final int BLUE = 0xff8080FF;
-
-        public MyAnimationView(Context context) {
-            super(context);
-
-            //背景颜色渐变动画，ObjectAnimator.ofInt，的用法
-
-            ValueAnimator colorAnim = ObjectAnimator.ofInt(this, "backgroundColor", RED, BLUE);
-            colorAnim.setDuration(3000);
-            colorAnim.setEvaluator(new ArgbEvaluator());
-            colorAnim.setRepeatCount(ValueAnimator.INFINITE);
-            colorAnim.setRepeatMode(ValueAnimator.REVERSE);
-            colorAnim.start();
-
-            /**
-             * 补充说明：Interpolator 控制动画的变化速率，译为插值器
-             * 加减速插值器 AccelerateDecelerateInterpolator
-             * 线性插值器 LinearInterpolator
-             * 加速插值器 AccelerateInterpolator
-             * 减速插值器 DecelerateInterpolator
-             *
-             */
+        public void startAnimation(){
+            //构建动画，执行动画
+            createAnimation();
+            animationSet.start();
         }
-
+        public void onAnimationUpdate(ValueAnimator animation) {
+            invalidate();
+        }
     }
+
 }
